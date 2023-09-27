1a20cecf82987c4aa05a96f56bba585e18546419
RomanRVV <rvachyov.roma@yandex.ru>
2023-09-28 01:36:04 +0300

    added authorization and registration

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 from users.models import User
 
@@ -15,3 +15,24 @@ class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ('username', 'password')
+
+
+class UserRegistrationForm(UserCreationForm):
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control'
+    }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'class': 'form-control'
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control'
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control'
+    }))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
