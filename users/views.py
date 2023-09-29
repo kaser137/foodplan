c95c4c586f7821db6283c8ab2b1e8ae6782df45f
Arkadiy <capark74@mail.ru>
2023-09-29 22:05:37 +0300

    Check get request

--- users/views.py
+++ users/views.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 
 from foodplan_site.functions import menu
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from users.models import User
+from foodplan_site.models import Order
 
 
 def login(request):
@@ -60,4 +62,52 @@ def logout(request):
 
 
 def order(request):
+    user = User.objects.get(username=request.user)
+    MONTH_PRICE = 300
+    foods_intolerances = ['Рыба и морепродукты', 'Мясо', 'Зерновые', 'Продукты пчеловодства', 'Орехи и бобовые', 'Молочные продукты']
+    context = {
+        'foods_intolerances': foods_intolerances,
+        'month_price': MONTH_PRICE,
+    }
+    if request.GET:
+        order_data = {
+            'month_duration': int(request.GET['month_duration']),
+            'breakfast': request.GET['select1'],
+            'lunch': request.GET['select2'],
+            'dinner': request.GET['select3'],
+            'dessert': request.GET['select4'],
+            'people_number': int(request.GET['select5']),
+            'intolerances': [],
+        }
+        print(order_data)
+        # for foods_intolerance in foods_intolerances:
+        #     foods_intolerance = str(foods_intolerance)
+        #     if foods_intolerance in request.GET:
+        #         intolerance_id = request.GET[foods_intolerance]
+        #         order_data['intolerances'].append(foods_intolerances.get(id=intolerance_id))
+
+        order = Order.objects.create(
+            user=user,
+            breakfast=order_data['breakfast'],
+            diner=order_data['dinner'],
+            supper=order_data['lunch'],
+            dessert=order_data['dessert'],
+            person=order_data['people_number'],
+            period=order_data['month_duration'],
+            # total_sum=round(MONTH_PRICE * order_data['month_duration'], 2),
+        )
+        # order.intolerance.set(order_data['intolerances'])
+        # description = f"""
+        #             Подписка на {order.month_count} месяц(а)
+        #             Аллергены: {[intolerance.name for intolerance in order_data['intolerances']]}
+        #             """
+        # return_url = f'http://{request.META["HTTP_HOST"]}/?' + urlencode({'order_id': order.id})
+
+        # payment = create_payment(ACCOUNT_ID, U_KEY, description, return_url, order)
+        # confirmation_url = payment.confirmation.confirmation_url
+
+        # order.yookassa_id = payment.id
+        # order.save()
+
+        # return redirect(confirmation_url)
     return render(request, 'users/order.html')
